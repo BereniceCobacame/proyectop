c2bd98d67d4a7081b1f50eea13b11f4d352ace1a
berenicecl <cobacameb@gmail.com>
2020-02-17 23:46:07 -0700

    first git commit with java project

---
+++ src/org/launchecode/Main.java
@@ -0,0 +1,12 @@
+package org.launchecode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String v = "Cinco";
+        int a = 5;
+
+        System.out.println(v+": "+a);
+    }
+}
