5853c4266e63d0215144d5eba6ed6a4d815392b2
berenicecl <cobacameb@gmail.com>
2020-02-18 23:59:09 -0700

    e v

--- src/org/launchecode/Main.java
+++ src/org/launchecode/Main.java
@@ -1,9 +1,17 @@
 package org.launchecode;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Perra");
+    public static void main(String[] args) throws IOException {
+        String var = System.getenv("PATHF");
+        FileInputStream fis = new FileInputStream(var);
+        Properties p = new Properties();
+        p.load(fis);
+
+        System.out.println(p.getProperty("usuario"));
     }
 }
